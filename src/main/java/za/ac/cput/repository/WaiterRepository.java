9ff521082727c2882ee62c59761aee7dc5dc79c5
Wareezy <warrenjaftha16@gmail.com>
2022-08-10 16:04:37 +0200

    Warren Jaftha-219005303-2nd commit-domain,factory and repository

--- src/main/java/za/ac/cput/repository/WaiterRepository.java
+++ src/main/java/za/ac/cput/repository/WaiterRepository.java
@@ -1,13 +1,14 @@
 package za.ac.cput.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import za.ac.cput.domain.Name;
 import za.ac.cput.domain.Waiter;
 
 import java.util.List;
 
 /*Warren Jaftha-219005303*/
-
+@Repository
 public interface WaiterRepository extends JpaRepository<Waiter,String> {
     public Waiter findByName(Name name);
 
